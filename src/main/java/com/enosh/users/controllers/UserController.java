0004175c51b1f560087ff7d7f72de91f8d4512f2
gilos <gil@sasover.com>
2020-06-12 11:09:14 +0300

    session

--- src/main/java/com/enosh/users/controllers/UserController.java
+++ src/main/java/com/enosh/users/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,23 +22,31 @@ public class UserController {
 
 	private final UserRepository userRepository;
 
+	private final String USER_KEY = "user";
+	
 	public UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
 
+	private boolean isLoggedIn(HttpServletRequest request) {
+		return request.getSession().getAttribute(USER_KEY) != null;
+	}
 	
 	// http://localhost:8080/user/get/1
 	
 	@GetMapping("/get/{id}")
-	public ResponseEntity<Response<User>> byId(@PathVariable("id") Long id) {
-		return userRepository
+	public ResponseEntity<Response<User>> byId(@PathVariable("id") Long id, HttpServletRequest request) {
+		return isLoggedIn(request) ? userRepository
 				.findById(id)
 				.map(user -> ResponseEntity.ok(
 						new Response<>(true,null,user)
 				))
 				.orElse(ResponseEntity.ok(
 						new Response<>(false, "No user by the id: " + id, null)
-				));
+				
+				)) : ResponseEntity.ok(
+						new Response<>(false, "Must be loggedin in order to continue", null)
+				);
 	}
 }
 
